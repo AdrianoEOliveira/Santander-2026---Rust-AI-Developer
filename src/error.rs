887f078a6620a0d7d61beee6653121dc3a6e8099
Adriano <Adrianoeo@outlook.com>
2026-08-13 13:11:51 -0300

    adiciona mais erros

--- src/error.rs
+++ src/error.rs
@@ -6,18 +6,40 @@ use thiserror::Error;
 pub enum AppError {
     #[error("Missing Authorization Headers")]
     MissingAuthorization,
+
+    #[error("Unauthorized access")]
+    Unauthorized,
+
     #[error("Invalid Credentials")]
     InvalidCredentials,
+
     #[error("Asset does not exist")]
     AssetDoesNotExist,
+
     #[error("User does not exist")]
     UserDoesNotExist,
+
     #[error("This username is already registered")]
     UsernameTaken,
+
+    #[error("Quantity must be greater than zero")]
+    InvalidQuantity,
+
+    #[error("Unit price must be greater than zero")]
+    InvalidUnitPrice,
+
+    #[error("Insufficient asset quantity for this operation")]
+    InsufficientQuantity,
+
+    #[error("Internal server error")]
+    InternalServerError,
+
     #[error(transparent)]
     Database(#[from] sqlx::Error),
+
     #[error(transparent)]
     Template(#[from] askama::Error),
+
     #[error(transparent)]
     Jwt(#[from] jwt_simple::Error),
 }
@@ -34,10 +56,17 @@ impl IntoResponse for AppError {
         };
 
         let status = match self {
-            Self::UsernameTaken | Self::MissingAuthorization => StatusCode::BAD_REQUEST,
-            Self::InvalidCredentials => StatusCode::UNAUTHORIZED,
+            Self::UsernameTaken
+            | Self::MissingAuthorization
+            | Self::InvalidQuantity
+            | Self::InvalidUnitPrice
+            | Self::InsufficientQuantity => StatusCode::BAD_REQUEST,
+
+            Self::InvalidCredentials | Self::Unauthorized => StatusCode::UNAUTHORIZED,
+
             Self::AssetDoesNotExist | Self::UserDoesNotExist => StatusCode::NOT_FOUND,
-            Self::Database(_) | Self::Template(_) | Self::Jwt(_) => {
+
+            Self::InternalServerError | Self::Database(_) | Self::Template(_) | Self::Jwt(_) => {
                 StatusCode::INTERNAL_SERVER_ERROR
             }
         };
